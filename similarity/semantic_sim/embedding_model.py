f401443da8b74edf16cd590cab2b9ff6e773ccb7
Yacine GACI <ey_gaci@esi.dz>
2020-03-06 11:40:42 +0100

    Renaming cosine_sim.py to embedding_model.py

--- similarity/semantic_sim/embedding_model.py
+++ similarity/semantic_sim/embedding_model.py
@@ -2,15 +2,11 @@ import numpy as np
 from sklearn.metrics.pairwise import cosine_similarity
 from gensim.models import Word2Vec
 
+from features import cosine_similarity
+
 
 def phrase_embedding(phrase):
     return np.mean([word_vectors[w] for w in phrase.split(' ') if w in word_vectors.vocab], axis=0)
-
-
-def compute_similarity(u, v):
-    u = u.reshape(1, u.shape[0])
-    v = v.reshape(1, v.shape[0])
-    return cosine_similarity(u, v)[0][0]
 
 
 model =  Word2Vec.load("models/word2vec.model")
@@ -26,4 +22,4 @@ if __name__ == "__main__":
     v1 = phrase_embedding(s1)
     v2 = phrase_embedding(s2)
 
-    print(compute_similarity(v1, v2))
+    print(cosine_similarity(v1, v2))
